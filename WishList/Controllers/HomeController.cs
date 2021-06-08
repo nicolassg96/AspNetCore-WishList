b0942b3f1865343b675d30640d654e16651b0738
nicolassg96 <83722269+nicolassg96@users.noreply.github.com>
2021-06-08 14:04:42 -0500

    Completed module

--- WishList/Controllers/HomeController.cs
+++ WishList/Controllers/HomeController.cs
@@ -8,5 +8,13 @@ namespace WishList.Controllers
 {
     public class HomeController: Controller
     {
+        public IActionResult Index()
+        {
+            return View("Index");
+        }
+        public IActionResult Error()
+        {
+            return View("Error");
+        }
     }
 }
